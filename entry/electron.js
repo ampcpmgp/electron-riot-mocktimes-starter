830f63c6926d2d9b99c82c766127592d0714a89d
ampcpmgp <ampcpmgp@gmail.com>
2018-12-06 00:26:06 +0900

    Electron starter (#1)
    * electron starter作成
    
    * ひとまず動かすところまで
    
    * 高さ調整
    
    * WIP
    
    * ある程度動いたところまで。
    
    * ある程度許容できる動きになったところまで
    
    * node.jsの結合ができたところまで。
    
    * ファイル整理
    
    * package.json 整理
    
    * 一部微調整

---
+++ entry/electron.js
@@ -0,0 +1,51 @@
+const electron = require('electron')
+
+const { app, BrowserWindow } = electron
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
+
+let mainWindow
+
+function createWindow () {
+  if (mainWindow) return
+
+  mainWindow = new BrowserWindow({
+    x: 10,
+    y: 100,
+    width: 1000,
+    height: 920,
+    'node-integration': false
+  })
+
+  const url = (function () {
+    if (process.env.NODE_ENV === 'develop') {
+      return `http://${require('ip').address()}:1234/pattern.html`
+    } else {
+      return 'dist/index.html'
+    }
+  })()
+
+  mainWindow.loadURL(url)
+
+  mainWindow.on('app-command', function (e, cmd) {
+    if (cmd === 'browser-backward' && mainWindow.webContents.canGoBack()) {
+      mainWindow.webContents.goToIndex(0)
+    }
+    if (cmd === 'browser-forward' && mainWindow.webContents.canGoForward()) {
+      mainWindow.webContents.goForward()
+    }
+  })
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+app.on('activate', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
