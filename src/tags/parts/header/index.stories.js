76434e626adcb115f1ced91be4b1afc5a95c4343
ampcpmgp <ampcpmgp@gmail.com>
2018-12-07 00:29:34 +0900

    https://github.com/ampcpmgp/parcel-riot-mocktimes-starter/pull/15 の取り込み

--- src/tags/parts/header/index.stories.js
+++ src/tags/parts/header/index.stories.js
@@ -1,11 +1,19 @@
 import { mount, storiesOf } from '@storybook/riot'
 import { withKnobs, number } from '@storybook/addon-knobs'
+import { withBackgrounds } from '@storybook/addon-backgrounds'
 import './index.tag'
 
 storiesOf('Header', module)
+  .addDecorator(
+    withBackgrounds([
+      { name: 'grey', value: '#ccc', default: true },
+      { name: 'lightgreen', value: 'lightgreen' }
+    ])
+  )
   .addDecorator(withKnobs)
   .add('normal', () =>
     mount('parts-header', {
-      testData: number('testData', 4)
+      handleMenuClick: console.info,
+      clickCount: number('clickCount', 0)
     })
   )
